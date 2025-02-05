af2cb1b8cd8293c92e0606ca3744119b8d4c5d1b
yukeshvenkadesh <127740041+yukeshxxd@users.noreply.github.com>
2025-02-05 12:25:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-set -e  # Stop script on error
-
-# Check if Docker is installed
-if ! command -v docker &> /dev/null; then
-    echo "⚠️ ERROR: Docker is not installed or not accessible!"
-    exit 1
-fi
-
-echo "✅ Docker is installed, proceeding..."
-echo hi123
+    echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u yukeshh -p 9095859072
     docker tag test yukeshh/docker_jenkins_task2
     docker push yukeshh/docker_jenkins_task2
+    
