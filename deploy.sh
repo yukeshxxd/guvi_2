87c77627d4947ec74ea0227eba96946ca427363c
yukeshvenkadesh <127740041+yukeshxxd@users.noreply.github.com>
2025-02-05 10:45:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,6 @@
-pipeline {
-    agent any
-
-    environment {
-        DOCKER_USERNAME = 'yukeshh'
-        DOCKER_PASSWORD = '9095859072'
-    }
-
-    stages {
-        stage('Build and Push Docker Image') {
-            steps {
-                echo 'hi123'
-                
-                // Make build.sh executable
-                sh 'chmod +x build.sh'
-                
-                // Run build script
-                sh './build.sh'
-                
-                // Log in to Docker
-                sh 'echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin'
-                
-                // Tag Docker image
-                sh 'docker tag test yukeshh/docker_jenkins_task2'
-                
-                // Push Docker image
-                sh 'docker push yukeshh/docker_jenkins_task2'
-            }
-        }
-    }
-}
+echo hi123
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u yukeshh -p 9095859072
+    docker tag test yukeshh/docker_jenkins_task2
+    docker push yukeshh/docker_jenkins_task2
