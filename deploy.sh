c95706a401ded033cd506cc1c8d83c024495a2dc
yukeshvenkadesh <127740041+yukeshxxd@users.noreply.github.com>
2025-02-05 10:53:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,6 @@
-#!/bin/bash
-
-echo "hi123"
-
-# Exit script on any error
-set -e
-
-# Ensure build.sh is executable
-chmod +x build.sh
-
-# Run build script
-./build.sh
-
-# Securely log in to Docker
-echo "Logging into Docker..."
-echo "9095859072" | docker login -u "yukeshh" --password-stdin
-
-# Tag Docker image
-docker tag test yukeshh/docker_jenkins_task2
-
-# Push Docker image
-docker push yukeshh/docker_jenkins_task2
-
-echo "Docker image pushed successfully!"
+echo hi123
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u yukeshh -p 9095859072
+    docker tag test yukeshh/docker_jenkins_task2
+    docker push yukeshh/docker_jenkins_task2
